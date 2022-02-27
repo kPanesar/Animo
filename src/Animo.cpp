b4e05938ba4a6bbb8bb125793a2cb14c09ebae47
Karanvir Panesar <singh.karanvir21@gmail.com>
2022-02-26 20:42:29 -0500

    Initial commit with basic window

---
+++ src/Animo.cpp
@@ -0,0 +1,153 @@
+#include "Animo.h"
+#include "imgui.h"
+#include "imgui_internal.h"
+
+namespace Animo
+{
+	static void HelpMarker(const char* desc)
+	{
+		ImGui::TextDisabled("(?)");
+		if (ImGui::IsItemHovered())
+		{
+			ImGui::BeginTooltip();
+			ImGui::PushTextWrapPos(ImGui::GetFontSize() * 35.0f);
+			ImGui::TextUnformatted(desc);
+			ImGui::PopTextWrapPos();
+			ImGui::EndTooltip();
+		}
+	}
+
+	void ResetDockSpace(ImGuiID dockspace_id, ImGuiViewport* viewport)
+	{
+		static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_PassthruCentralNode;
+
+		static auto first_time = true;
+		if (first_time)
+		{
+			first_time = false;
+
+			ImGui::DockBuilderRemoveNode(dockspace_id); // clear any previous layout
+			ImGui::DockBuilderAddNode(dockspace_id, dockspace_flags | ImGuiDockNodeFlags_DockSpace);
+			ImGui::DockBuilderSetNodeSize(dockspace_id, viewport->Size);
+
+			auto dock_id_left = ImGui::DockBuilderSplitNode(dockspace_id, ImGuiDir_Left, 0.25f, nullptr, &dockspace_id);
+			auto dock_id_left_down = ImGui::DockBuilderSplitNode(dock_id_left, ImGuiDir_Down, 0.5f, nullptr, &dock_id_left);
+
+			ImGui::DockBuilderDockWindow("Viewport", dockspace_id); // Main
+			ImGui::DockBuilderDockWindow("Documents", dock_id_left);
+			ImGui::DockBuilderDockWindow("Codes", dock_id_left_down);
+			ImGui::DockBuilderFinish(dockspace_id);
+		}
+	}
+
+	// Note that shortcuts are currently provided for display only
+    // (future version will add explicit flags to BeginMenu() to request processing shortcuts)
+    static void ShowMenuBar()
+    {
+		if (ImGui::BeginMenuBar())
+        {
+            if (ImGui::BeginMenu("File"))
+            {
+                if (ImGui::MenuItem("New")) {}
+				if (ImGui::MenuItem("Open", "Ctrl+O")) {}
+				if (ImGui::BeginMenu("Open Recent"))
+				{
+					ImGui::MenuItem("fish_hat.c");
+					ImGui::MenuItem("fish_hat.inl");
+					ImGui::MenuItem("fish_hat.h");
+					if (ImGui::BeginMenu("More.."))
+					{
+						ImGui::MenuItem("Hello");
+						ImGui::MenuItem("Sailor");
+						ImGui::EndMenu();
+					}
+					ImGui::EndMenu();
+				}
+				if (ImGui::MenuItem("Save", "Ctrl+S")) {}
+				if (ImGui::MenuItem("Save As..")) {}
+
+				ImGui::Separator();
+
+				// Here we demonstrate appending again to the "Options" menu (which we already created above)
+				// Of course in this demo it is a little bit silly that this function calls BeginMenu("Options") twice.
+				// In a real code-base using it would make senses to use this feature from very different code locations.
+				if (ImGui::BeginMenu("Options")) // <-- Append!
+				{
+					static bool b = true;
+					ImGui::Checkbox("SomeOption", &b);
+					ImGui::EndMenu();
+				}
+
+				if (ImGui::BeginMenu("Disabled", false)) // Disabled
+				{
+					IM_ASSERT(0);
+				}
+				if (ImGui::MenuItem("Checked", NULL, true)) {}
+				if (ImGui::MenuItem("Quit", "Alt+F4")) {}
+                ImGui::EndMenu();
+            }
+            ImGui::EndMenuBar();
+        }
+    }
+
+    void RenderUI()
+    {
+		static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_PassthruCentralNode;
+		ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+
+		ImGuiViewport* viewport = ImGui::GetMainViewport();
+		ImGui::SetNextWindowPos(viewport->Pos);
+		ImGui::SetNextWindowSize(viewport->Size);
+		ImGui::SetNextWindowViewport(viewport->ID);
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+		window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+		window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+
+
+		// When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will render our background and handle the pass-thru hole, so we ask Begin() to not render a background.
+		if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
+		window_flags |= ImGuiWindowFlags_NoBackground;
+
+		// Important: note that we proceed even if Begin() returns false (aka window is collapsed).
+		// This is because we want to keep our DockSpace() active. If a DockSpace() is inactive, 
+		// all active windows docked into it will lose their parent and become undocked.
+		// We cannot preserve the docking relationship between an active window and an inactive docking, otherwise 
+		// any change of dockspace/settings would lead to windows being stuck in limbo and never being visible.
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+		ImGui::Begin("DockSpace", nullptr, window_flags);
+		ImGui::PopStyleVar();
+		ImGui::PopStyleVar(2);
+
+		ShowMenuBar();
+
+		ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
+		ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
+		ImGuiWindowSettings* settings = ImGui::FindWindowSettings(ImGui::GetCurrentWindow()->ID);
+		if (!settings)
+        {
+			ResetDockSpace(dockspace_id, viewport);
+		}
+
+		ImGui::Begin("Documents", 0);
+		const char* items[] = { "Apple", "Banana", "Cherry", "Kiwi", "Mango", "Orange", "Pineapple", "Strawberry", "Watermelon" };
+		static int item_current = 1;
+		ImGui::ListBox("Document List", &item_current, items, IM_ARRAYSIZE(items), 4);
+		ImGui::SameLine(); HelpMarker(
+                "Using the simplified one-liner ListBox API here.\nRefer to the \"List boxes\" section below for an explanation of how to use the more flexible and general BeginListBox/EndListBox API.");
+        ImGui::End();
+
+		ImGui::Begin("Codes", 0);
+        ImGui::Button("Animo");
+        static float value = 0.0f;
+        ImGui::DragFloat("Value", &value);
+        ImGui::End();
+
+
+		ImGui::Begin("Viewport", 0);
+        ImGui::End();
+		
+		//ImGui::ShowDemoWindow();
+        ImGui::End();
+    }
+}
